0b7101508e5c801d4f086768dc20e3f201016c1f
unknown <padaveamol.12@gmail.com>
2024-06-12 11:56:19 +0530

    removed pycache files

--- food_order_system/food_order_proj/settings.py
+++ food_order_system/food_order_proj/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'foodsystem',
         'USER': 'root',
-        'PASSWORD': 'Red@77199',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
